1cc36ac79ed802908b9b8b879a8b6c164718ebd5
dcarabis <dcarabis@users.noreply.github.com>
2018-07-31 09:58:51 -0400

    Create MCFgen_baxter.m
    generates joint-space mass-inertia, coriolis/centrifugal, and friction terms for rigid joint dynamic model

---
+++ subfunctions/MCFgen_baxter.m
@@ -0,0 +1,74 @@
+function [MassInertia,Cq,F] = MCFgen_baxter(q,qdot,robot_const,V0)
+
+% inputs:
+% q: joint positions
+% robot_kin: robot parameters structure
+% V0: base veloicty
+
+% Extract info from robot parameters structure
+h = robot_const.kin.H; 
+p = robot_const.kin.P;
+I = robot_const.mprops.I;
+m = robot_const.mprops.m;
+c = robot_const.mprops.c;
+N = robot_const.mprops.N;
+
+% Generate H, PHI block matrices
+
+
+H1 = [h(:,1); 0;0;0]; H2 = [h(:,2); 0;0;0]; H3 = [h(:,3); 0;0;0]; 
+H4 = [h(:,4); 0;0;0]; H5 = [h(:,5); 0;0;0]; H6 = [h(:,6); 0;0;0]; 
+H7 = [h(:,7); 0;0;0];
+
+H = [H1 zeros(6,6); zeros(6,1) H2 zeros(6,5); zeros(6,2) H3 zeros(6,4); ...
+    zeros(6,3) H4 zeros(6,3); zeros(6,4) H5 zeros(6,2); zeros(6,5) H6 ...
+    zeros(6,1); zeros(6,6) H7];
+
+PHI = phi_gen(q,h,p);
+
+V = zeros(6,7);
+Vtemp = V0;
+Vprev = V0;
+a = zeros(6,7);
+b = zeros(6,7);
+M = zeros(6,6,7);
+
+for k = 1:7
+    
+   PHI_TEMP = PHI((6*(k) - 5):(6*(k)),(6*(k) - 5):(6*(k)));
+   Htemp = H((6*(k) - 5):(6*(k)),k);
+   Vtemp = PHI_TEMP*Vtemp + Htemp*qdot(k);
+   V(:,k) = Vtemp;
+   
+   R = rot(H(:,k),q(k));
+   
+   a(:,k) = [crossmat(R'*Vprev(1:3))*(Vtemp(1:3));...
+        R'*crossmat(Vprev(1:3))*(crossmat(Vprev(1:3))*p(:,k))];
+   Vprev = Vtemp;
+   
+   b(:,k) = [crossmat(Vtemp(1:3))*I(:,:,k)*Vtemp(1:3); ...
+        m(k)*crossmat(Vtemp(1:3))*crossmat(Vtemp(1:3))*c(:,k)];
+    
+    M(:,:,k) =  [I(:,:,k) m(k)*crossmat(c(:,k)); ...
+        -m(k)*crossmat(c(:,k)) m(k)*eye(3,3)];
+end
+
+PHI = PHI(:,7:end);
+
+% Solve for Mass Inertia matrix
+
+M_NE = zeros(42,42);
+M_NE(1:6,1:6) = M(:,:,1); M_NE(7:12,7:12) = M(:,:,2); 
+M_NE(13:18,13:18) = M(:,:,3); M_NE(19:24,19:24) = M(:,:,4);
+M_NE(25:30,25:30) = M(:,:,5); M_NE(31:36,31:36) = M(:,:,6); 
+M_NE(37:42,37:42) = M(:,:,7);
+
+MassInertia = H'*PHI'*M_NE*PHI*H;
+
+% Solve for Coriolis/Centrifugal terms (C*qdot)
+Cq = H'*PHI'*(M_NE*PHI*a(:) + b(:));
+
+% Solve for Friction Terms
+F = -friction_adjust_Baxter(zeros(7,1),N.*qdot);
+
+end
